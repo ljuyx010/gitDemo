f66fc443ad29efdfd201597b20ccd5fd9667321f
ljuyx010 <34336224+ljuyx010@users.noreply.github.com>
2025-08-15 23:46:03 +0800

    增加数据类型章节

--- python_code/new.py
+++ python_code/new.py
@@ -22,18 +22,50 @@ name = "张三"
 # 10进制
 print(10)
 
-#16进制
+# 16进制
 print(0x10)
 
-#8进制
+# 8进制
 print(0o10)
 
-#2进制
+# 2进制
 print(0b10)
 
-p=2**100
+p = 2 ** 100
 # 在python中可以通过sys.getsizeof(obj)返回obj的大小（按照字节单位返回）
 print(sys.getsizeof(p))
 
 print(sys.float_info.max)
 print(sys.float_info.min)
+
+# 布尔类型
+num1 = 100
+num2 = 200
+if num1 > num2:
+    print("num>num2")
+result = num1 > num2
+print("result=", result)
+
+# 原样输出字符串
+cont = """
+def any(iterable):
+    for element in iterable:
+        if element:
+            return True
+    return False
+"""
+print(cont)
+str = r"jack \n tom \t jeron"
+print(str)
+
+str1 = "hello"
+str2 = "hello"
+str3 = "hello"
+# id()函数，可以返回对象/数据的内存地址
+print("str1的地址：", id(str1))
+print("str2的地址：", id(str2))
+print("str3的地址：", id(str3))
+
+str1 = "abc#"
+str2 = "abc#"
+print(id(str1) == id(str2))
