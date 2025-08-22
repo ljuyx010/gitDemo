ccce6e653e1d2cffb28e3a714ac484275558a3c6
ljuyx <649047079@qq.com>
2025-08-22 18:03:02 +0800

    完成集合章节

---
+++ python_code/list_create.py
@@ -0,0 +1,16 @@
+# @version : 1.0
+# @Author  : 混江龙
+# @File    : list_create.py
+# @Time    : 2025/8/22 10:33
+
+#用列表生成式生成内容为[1,4,9,16,25,36,49,64,81,100]
+# lst = [ele**2 for ele in range(1,11)]
+# print("lst = ", lst)
+
+# lst = [float(input("请输入成绩:")) for i in range(5)]
+# print("成绩为：", lst)
+
+tuple_a=(100,)
+tuple_a1=(100)
+print(type(tuple_a))
+print(type(tuple_a1))
