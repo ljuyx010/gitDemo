4763ad7d495b9b82f4df56db68537da8ce1e9fcf
ljuyx010 <34336224+ljuyx010@users.noreply.github.com>
2025-08-26 22:42:36 +0800

    完成self章节

---
+++ python_code/li_package/__init__.py
@@ -0,0 +1,4 @@
+# @version : 1.0
+# @Author  : 混江龙
+# @File    : __init__.py.py
+# @Time    : 2025/8/26 15:02
---
+++ python_code/li_package/moudle01.py
@@ -0,0 +1,5 @@
+# @version : 1.0
+# @Author  : 混江龙
+# @File    : moudle01.py
+# @Time    : 2025/8/26 15:44
+import  requests
