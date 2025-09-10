a95061038f8a9639bd06502f626ecfe173725894
ljuyx010 <34336224+ljuyx010@users.noreply.github.com>
2025-09-10 22:23:14 +0800

    java第一课

---
+++ java_code/test.java
@@ -0,0 +1,8 @@
+//最简单的程序，完成1+1 体会
+
+public class Test{
+	public static void main(String[] args){
+		int res = 1+1;
+		System.out.println("结果="+res);
+	}
+}
---
+++ python_code/tk02.py
@@ -0,0 +1,4 @@
+# @version : 1.0
+# @Author  : 混江龙
+# @File    : tk02.py
+# @Time    : 2025/9/9 20:48
